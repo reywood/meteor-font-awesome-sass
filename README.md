de22fa4b5ca37e6dca20fdbd0ae17d139a40252e
Sean Dwyer <sean.dwyer@gmail.com>
2014-08-29 12:49:27 -0700

    Fix completely broken meteor 0.9 packaging attempt

--- README.md
+++ README.md
@@ -6,11 +6,8 @@ Font Awesome stylesheets in SCSS format packaged for Meteor
 To install
 ----------
 
-In addition to the font-awesome-sass package, you must explicitly install the scss package. Meteor will not compile your `*.scss` files otherwise.
-
 ```sh
 $ meteor add mrt:font-awesome-sass
-$ meteor add fourseven:scss
 ```
 
 To use
@@ -19,7 +16,7 @@ To use
 To activate Font Awesome styles on your site, add the following line to the top of your main scss file:
 
 ```scss
-@import 'packages/font-awesome-sass/font-awesome';
+@import 'packages/mrt:font-awesome-sass/font-awesome';
 ```
 
 --------------------------------------------------------
--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
     name: "mrt:font-awesome-sass",
     summary: "Font Awesome stylesheets in SCSS format",
-    version: "4.1.4",
+    version: "4.1.5",
     git: "https://github.com/reywood/meteor-font-awesome-sass.git"
 });
 
 Package.on_use(function (api) {
     api.versionsFrom("METEOR@0.9.0");
-    api.use(["fourseven:scss@0.9.5"]);
+
+    api.use(["fourseven:scss@0.9.5"], ["server"]);
+    api.imply("fourseven:scss@0.9.5", ["server"]);
 
     api.add_files([
         "assets/fonts/fontawesome-webfont.eot",
@@ -16,6 +18,22 @@ Package.on_use(function (api) {
         "assets/fonts/fontawesome-webfont.woff",
         "assets/fonts/FontAwesome.otf"
     ], "client");
+
+    api.add_files([
+        "_font-awesome.scss",
+        "assets/scss/_bordered-pulled.scss",
+        "assets/scss/_core.scss",
+        "assets/scss/_fixed-width.scss",
+        "assets/scss/_icons.scss",
+        "assets/scss/_larger.scss",
+        "assets/scss/_list.scss",
+        "assets/scss/_mixins.scss",
+        "assets/scss/_path.scss",
+        "assets/scss/_rotated-flipped.scss",
+        "assets/scss/_spinning.scss",
+        "assets/scss/_stacked.scss",
+        "assets/scss/_variables.scss"
+    ], "server");
 });
 
 Package.on_test(function(api) {
